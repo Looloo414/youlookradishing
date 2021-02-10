3dc728508d0e7ca7dc10cea31683a2ddb9c51350
Looloo414 <alexafaulkison@gmail.com>
2021-02-09 20:14:31 -0700

    Sign up/login

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
@@ -8,3 +13,23 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+# --------------SIGN UP/LOGIN-------------------------
+def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
