37208f2d01d11fa2093856bc3ffe23ecce16bb07
Looloo414 <alexafaulkison@gmail.com>
2021-02-09 20:19:42 -0700

    Workout Model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date, datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Workout(models.Model):
+    activity = models.CharField(max_length=100)
+    howLong = models.IntegerField()
+    description = models.TextField(max_length=300)
+    time = models.DateTimeField('Workout Time')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=User)
+
+    def __str__(self):
+        return self.activity
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'workout_id': self.id})
